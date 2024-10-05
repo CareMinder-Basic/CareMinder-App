d7a4fa4fb3bcd14db3699ded0031f748963b7bb5
LeeSeungHun <dos07008@naver.com>
2024-10-05 18:57:07 +0900

    feat: 요청사항 컴포넌트 개발

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,76 +1,13 @@
-import { ScrollView, View } from "react-native";
-import { Button, Text } from "@rneui/themed";
 import { Layout } from "@/components/Layout";
-import { router } from "expo-router";
+import { View } from "react-native";
+import { RequestSection } from "./modules/components/RequestSection";
 
 export default function HomeScreen() {
   return (
     <Layout>
-      <ScrollView
-        style={{
-          backgroundColor: "gray",
-        }}
-      >
-        <Button
-          font="Pretendard_100"
-          onPress={() => {
-            router.push("./sign-in/nurse");
-          }}
-        >
-          로그인 페이지
-        </Button>
-
-        <Button
-          buttonStyle={{ backgroundColor: "#5D6DBE" }}
-          font="Pretendard_100"
-          onPress={() => {
-            router.push("./cv-intro/intro");
-          }}
-        >
-          케어보이스 소개
-        </Button>
-
-        <Button
-          buttonStyle={{ backgroundColor: "#5D6DBE" }}
-          font="Pretendard_100"
-          onPress={() => {
-            router.push("/sign-in/setting-info");
-          }}
-        >
-          구역 설정 페이지
-        </Button>
-
-        <Button
-          buttonStyle={{ backgroundColor: "#BEE7FF" }}
-          font="Pretendard_100"
-          onPress={() => {
-            router.push("/sign-in/patient");
-          }}
-        >
-          환자 로그인 페이지
-        </Button>
-
-        <Button
-          buttonStyle={{ backgroundColor: "#5D6DBE" }}
-          font="Pretendard_100"
-          onPress={() => {
-            router.push("/user-guide/guide");
-          }}
-        >
-          유저 가이드
-        </Button>
-
-        <View style={{ backgroundColor: "blue", minHeight: 800 }}>
-          <Text font={"Pretendard_700"} color={"#F24679"}>
-            123213
-          </Text>
-        </View>
-        <View style={{ backgroundColor: "white", minHeight: 800 }}>
-          <Text font={"Pretendard_700"} color={"#F24679"}>
-            44444
-          </Text>
-        </View>
-      </ScrollView>
+      <View style={{ flex: 1 }}>
+        <RequestSection />
+      </View>
     </Layout>
   );
 }
---
+++ app/(tabs)/modules/components/RequestSection.tsx
@@ -0,0 +1,85 @@
+import { UnionSvg } from "@/components/svgs/Union";
+import { Text, useTheme } from "@rneui/themed";
+import { LinearGradient } from "expo-linear-gradient";
+import { Pressable, View } from "react-native";
+
+interface VerticalLineProps {
+  height: number;
+}
+
+function VerticalLine({ height }: VerticalLineProps) {
+  return (
+    <View
+      style={{
+        backgroundColor: "#8e9bdb",
+        borderRadius: 100,
+        height,
+        width: 12,
+      }}
+    />
+  );
+}
+
+export function RequestSection() {
+  const { theme } = useTheme();
+  return (
+    <LinearGradient
+      style={{
+        alignItems: "center",
+        gap: 32,
+        height: "100%",
+        justifyContent: "center",
+        width: "60%",
+      }}
+      start={[0, 0]}
+      end={[0, 1]}
+      colors={[theme.colors["#5D6DBE"], theme.colors["#607AFF"]]}
+    >
+      <View style={{ alignItems: "center", flexDirection: "row", gap: 18 }}>
+        <View style={{ alignItems: "center", flexDirection: "row", gap: 16 }}>
+          <VerticalLine height={40} />
+          <VerticalLine height={62} />
+          <VerticalLine height={98} />
+          <VerticalLine height={62} />
+        </View>
+        <UnionSvg />
+        <View style={{ alignItems: "center", flexDirection: "row", gap: 16 }}>
+          <VerticalLine height={62} />
+          <VerticalLine height={98} />
+          <VerticalLine height={62} />
+          <VerticalLine height={40} />
+        </View>
+      </View>
+      <Text
+        font="Pretendard_700"
+        color="#FFFFFF"
+        style={{ fontSize: 40, lineHeight: 72 }}
+      >
+        요청사항을 말씀해주세요
+      </Text>
+      <Text
+        font="Pretendard_500"
+        color="#FFFFFF"
+        style={{ fontSize: 20, lineHeight: 30 }}
+      >
+        자동으로 간호사에게 전달됩니다.
+      </Text>
+      <Pressable
+        style={{
+          backgroundColor: theme.colors["#000000"],
+          borderRadius: 100,
+          paddingHorizontal: 44,
+          paddingVertical: 14,
+        }}
+      >
+        <Text
+          font="Pretendard_500"
+          color="#FFFFFF"
+          style={{ fontSize: 30, lineHeight: 30 }}
+        >
+          음성 요청하기
+        </Text>
+      </Pressable>
+    </LinearGradient>
+  );
+}
